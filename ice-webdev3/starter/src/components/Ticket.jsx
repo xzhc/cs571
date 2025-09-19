d9e045857b2f663609b229da64d41f5f847861ca
xzhc <1838998336@qq.com>
2025-09-19 16:40:53 +0800

    manage state by props

--- ice-webdev3/starter/src/components/Ticket.jsx
+++ ice-webdev3/starter/src/components/Ticket.jsx
@@ -1,12 +1,33 @@
 import { Button, Card } from "react-bootstrap";
 
 const Ticket = (props) => {
-    return <Card style={{margin: "0.25rem"}}>
-        <h2 style={{fontSize: "1.25rem"}}>{props.name}</h2>
-        <sub>{props.author}</sub>
-        <br/>
-        <p>{props.description}</p>
+  const markTodo = () => {
+    props.move(props.id, props.status, "todo");
+  };
+
+  const markInProgress = () => {
+    props.move(props.id, props.status, "inprogress");
+  };
+  const markDone = () => {
+    props.move(props.id, props.status, "done");
+  };
+  return (
+    <Card style={{ margin: "0.25rem" }}>
+      <h2 style={{ fontSize: "1.25rem" }}>{props.name}</h2>
+      <sub>{props.author}</sub>
+      <br />
+      <p>{props.description}</p>
+      <Button variant="secondary" onClick={markTodo}>
+        ToDo
+      </Button>
+      <Button variant="primary" onClick={markInProgress}>
+        In Progress
+      </Button>
+      <Button variant="success" onClick={markDone}>
+        Done
+      </Button>
     </Card>
-}
+  );
+};
 
 export default Ticket;
--- ice-webdev3/starter/src/components/TicketBoard.jsx
+++ ice-webdev3/starter/src/components/TicketBoard.jsx
@@ -1,47 +1,69 @@
 import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 
-import TicketLane from './TicketLane'
+import TicketLane from "./TicketLane";
 
 const TicketBoard = (props) => {
+  const [ticketLanes, setTicketLanes] = useState({
+    todo: [],
+    inprogress: [],
+    done: [],
+  });
 
-    const [ticketLanes, setTicketLanes] = useState({
-        todo: [],
-        inprogress: [],
-        done: [],
+  useEffect(() => {
+    fetch("https://cs571.org/rest/s25/ice/tickets", {
+      headers: {
+        "X-CS571-ID": CS571.getBadgerId(),
+      },
     })
+      .then((res) => res.json())
+      .then((ticketData) => {
+        console.log(ticketData);
+        setTicketLanes({
+          todo: ticketData,
+          inprogress: [],
+          done: [],
+        });
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch('https://cs571api.cs.wisc.edu/rest/s25/ice/tickets', {
-            headers: {
-                "X-CS571-ID": CS571.getBadgerId()
-            }
-        })
-        .then(res => res.json())
-        .then(ticketData => {
-            console.log(ticketData);
-            setTicketLanes({
-                todo: ticketData,
-                inprogress: [],
-                done: []
-            });
-        })
-    }, []);
+  const move = (tickedId, from, to) => {
+    console.log(`Moving ${tickedId} from ${from} to ${to}`);
+    if (from === to) {
+      return;
+    }
 
-    return <div>
-        <h1>Ticket Board</h1>
-        <Container fluid>
-            {
-                Object.keys(ticketLanes).map(laneName => {
-                    return <TicketLane
-                        key={laneName}
-                        status={laneName}
-                        tickets={ticketLanes[laneName]}
-                    />
-                })
-            }
-        </Container>
+    setTicketLanes((oldLanes) => {
+      let fromLane = oldLanes[from];
+      let toLane = oldLanes[to];
+
+      const ticketToMove = fromLane.find((ticket) => ticket.id === tickedId);
+
+      const newLanes = { ...oldLanes };
+      newLanes[from] = fromLane.filter((ticket) => ticket.id !== tickedId);
+      newLanes[to] = [...toLane, ticketToMove];
+
+      return newLanes;
+    });
+  };
+
+  return (
+    <div>
+      <h1>Ticket Board</h1>
+      <Container fluid>
+        {Object.keys(ticketLanes).map((laneName) => {
+          return (
+            <TicketLane
+              move={move}
+              key={laneName}
+              status={laneName}
+              tickets={ticketLanes[laneName]}
+            />
+          );
+        })}
+      </Container>
     </div>
-}
+  );
+};
 
 export default TicketBoard;
