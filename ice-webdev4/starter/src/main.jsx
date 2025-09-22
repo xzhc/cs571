9e85adf794a8a567a60d7adb1f34b800235ac4b2
xzhc <1838998336@qq.com>
2025-09-22 14:10:17 +0800

    ice-webdev4 starter

---
+++ ice-webdev4/starter/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CommentBoard from './components/CommentBoard'
+
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <CommentBoard />
+)
